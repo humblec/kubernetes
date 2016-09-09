f6aa54d42776869ba668dd97ef4b2049caf22120
Humble Chirammal <hchiramm@redhat.com>
2016-09-09 20:53:44 +0530

    Introduce endpointsnamespace in glusterfsvolumesource
    Signed-off-by: Humble Chirammal <hchiramm@redhat.com>

--- pkg/volume/glusterfs/glusterfs.go
+++ pkg/volume/glusterfs/glusterfs.go
@@ -55,6 +55,8 @@ var _ volume.Deleter = &glusterfsVolumeDeleter{}
 const (
 	glusterfsPluginName = "kubernetes.io/glusterfs"
 	volprefix           = "vol_"
+	defaultendpointns   = "default"
+	epsvcprefix = "cluster-"
 )
 
 func (plugin *glusterfsPlugin) Init(host volume.VolumeHost) error {
@@ -73,8 +75,9 @@ func (plugin *glusterfsPlugin) GetVolumeName(spec *volume.Spec) (string, error) 
 	}
 
 	return fmt.Sprintf(
-		"%v:%v",
+		"%v:%v:%v",
 		volumeSource.EndpointsName,
+		volumeSource.EndpointsNameSpace,
 		volumeSource.Path), nil
 }
 
@@ -102,14 +105,18 @@ func (plugin *glusterfsPlugin) GetAccessModes() []api.PersistentVolumeAccessMode
 func (plugin *glusterfsPlugin) NewMounter(spec *volume.Spec, pod *api.Pod, _ volume.VolumeOptions) (volume.Mounter, error) {
 	source, _ := plugin.getGlusterVolumeSource(spec)
 	ep_name := source.EndpointsName
-	ns := pod.Namespace
+	ep_namespace := source.EndpointsNameSpace
+	//TODO
+	//ns := pod.Namespace
+	//ns := ep_namespace
+	ns := defaultendpointns
 	ep, err := plugin.host.GetKubeClient().Core().Endpoints(ns).Get(ep_name)
-	if err != nil {
+	if err != nil && len(ep_namespace) == 0 {
 		glog.Errorf("glusterfs: failed to get endpoints %s[%v]", ep_name, err)
 		return nil, err
 	}
-	glog.V(1).Infof("glusterfs: endpoints %v", ep)
-	return plugin.newMounterInternal(spec, ep, pod, plugin.host.GetMounter(), exec.New())
+	glog.V(2).Infof("glusterfs: endpoints %v ep_namespace %v", ep, ep_namespace)
+	return plugin.newMounterInternal(spec, ep, pod, plugin.host.GetMounter(), exec.New(), ep_namespace)
 }
 
 func (plugin *glusterfsPlugin) getGlusterVolumeSource(spec *volume.Spec) (*api.GlusterfsVolumeSource, bool) {
@@ -122,7 +129,7 @@ func (plugin *glusterfsPlugin) getGlusterVolumeSource(spec *volume.Spec) (*api.G
 	}
 }
 
-func (plugin *glusterfsPlugin) newMounterInternal(spec *volume.Spec, ep *api.Endpoints, pod *api.Pod, mounter mount.Interface, exe exec.Interface) (volume.Mounter, error) {
+func (plugin *glusterfsPlugin) newMounterInternal(spec *volume.Spec, ep *api.Endpoints, pod *api.Pod, mounter mount.Interface, exe exec.Interface, ep_namespace string) (volume.Mounter, error) {
 	source, readOnly := plugin.getGlusterVolumeSource(spec)
 	return &glusterfsMounter{
 		glusterfs: &glusterfs{
@@ -132,6 +139,7 @@ func (plugin *glusterfsPlugin) newMounterInternal(spec *volume.Spec, ep *api.End
 			plugin:  plugin,
 		},
 		hosts:    ep,
+		endpointsns:  ep_namespace,
 		path:     source.Path,
 		readOnly: readOnly,
 		exe:      exe}, nil
@@ -161,6 +169,7 @@ func (plugin *glusterfsPlugin) ConstructVolumeSpec(volumeName, mountPath string)
 		VolumeSource: api.VolumeSource{
 			Glusterfs: &api.GlusterfsVolumeSource{
 				EndpointsName: volumeName,
+				EndpointsNameSpace: volumeName,
 				Path:          volumeName,
 			},
 		},
@@ -180,6 +189,7 @@ type glusterfs struct {
 type glusterfsMounter struct {
 	*glusterfs
 	hosts    *api.Endpoints
+	endpointsns string
 	path     string
 	readOnly bool
 	exe      exec.Interface
@@ -262,7 +272,6 @@ func (c *glusterfsUnmounter) cleanup(dir string) error {
 			return fmt.Errorf("glusterfs: RemoveAll failed: %v", err)
 		}
 	}
-
 	return nil
 }
 
@@ -285,24 +294,31 @@ func (b *glusterfsMounter) setUpAtInternal(dir string) error {
 	log := path.Join(p, b.pod.Name+"-glusterfs.log")
 	options = append(options, "log-level=ERROR")
 	options = append(options, "log-file="+log)
-
-	addr := make(map[string]struct{})
-	for _, s := range b.hosts.Subsets {
-		for _, a := range s.Addresses {
-			addr[a.IP] = struct{}{}
+	var addrlist []string
+	if b.hosts != nil || b.endpointsns != "" {
+		addr := make(map[string]struct{})
+		if b.hosts.Subsets != nil {
+			for _, s := range b.hosts.Subsets {
+				for _, a := range s.Addresses {
+					addr[a.IP] = struct{}{}
+					addrlist = append(addrlist, a.IP)
+				}
+			}
+		} else {
+			//addrlist = b.servers
+			glog.V(1).Infof("glusterfs: both endpoint and endpointsNameSpace are nil")
+			return fmt.Errorf("glusterfs: both endpoint and endpointsNameSpace are nil")
+		}
+		// Avoid mount storm, pick a host randomly.
+		// Iterate all hosts until mount succeeds.
+		for _, ip := range addrlist {
+			errs = b.mounter.Mount(ip+":"+b.path, dir, "glusterfs", options)
+			if errs == nil {
+				glog.Infof("glusterfs: successfully mounted %s", dir)
+				return nil
+			}
 		}
 	}
-
-	// Avoid mount storm, pick a host randomly.
-	// Iterate all hosts until mount succeeds.
-	for hostIP := range addr {
-		errs = b.mounter.Mount(hostIP+":"+b.path, dir, "glusterfs", options)
-		if errs == nil {
-			glog.Infof("glusterfs: successfully mounted %s", dir)
-			return nil
-		}
-	}
-
 	// Failed mount scenario.
 	// Since gluster does not return eror text
 	// it all goes in a log file, we will read the log file
@@ -400,12 +416,11 @@ func (d *glusterfsVolumeDeleter) Delete() error {
 	}
 	err = cli.VolumeDelete(newvolumetodel)
 	if err != nil {
-		glog.V(4).Infof("glusterfs: error when deleting the volume :%s", err)
+		glog.Errorf("glusterfs: error when deleting the volume :%s", err)
 		return err
 	}
 	glog.V(2).Infof("glusterfs: volume %s deleted successfully", volumetodel)
 	return nil
-
 }
 
 func (r *glusterfsVolumeProvisioner) Provision() (*api.PersistentVolume, error) {
@@ -474,9 +489,74 @@ func (p *glusterfsVolumeProvisioner) CreateVolume() (r *api.GlusterfsVolumeSourc
 		return nil, 0, fmt.Errorf("error creating volume %v", err)
 	}
 	glog.V(1).Infof("glusterfs: volume with size :%d and name:%s created", volume.Size, volume.Name)
+	clusterinfo, err := cli.ClusterInfo(volume.Cluster)
+	var dhostlistf []string
+	for _, node := range clusterinfo.Nodes {
+			nodei, err := cli.NodeInfo(string(node))
+			if err != nil {
+				glog.Errorf("glusterfs: failed to get hostip %s ", err)
+				return nil,0, fmt.Errorf("failed to get hostip %v", err)
+			}
+			ipaddr := dstrings.Join(nodei.NodeAddRequest.Hostnames.Storage,"")
+			dhostlistf = append(dhostlistf, ipaddr)
+	}
+	glog.V(1).Infof("glusterfs: hostlist :\n%v\n", dhostlistf)
+	if len(dhostlistf) == 0 {
+		glog.Errorf("glusterfs: no endpoint hosts found %s ", err)
+		return nil, 0, fmt.Errorf("no endpoint hosts found %v", err)
+	}
+	depsertoc := epsvcprefix+volume.Cluster
+	dep, dser, err := p.CreateEpSvc(defaultendpointns, depsertoc, dhostlistf)
+	if err != nil {
+		glog.Errorf("glusterfs: failed to create endpoint/service")
+		return nil, 0, fmt.Errorf("failed to create endpoint/service %v", err)
+	}
+	glog.V(1).Infof("glusterfs: dynamic ep %#v and svc : %#v ", dep, dser)
 	return &api.GlusterfsVolumeSource{
-		EndpointsName: p.glusterfsClusterConf.glusterep,
+		//EndpointsName: p.glusterfsClusterConf.glusterep,
 		Path:          volume.Name,
+		EndpointsName: dep.Name,
+		EndpointsNameSpace: defaultendpointns,
+		//Path:          "myVol1",
 		ReadOnly:      false,
 	}, sz, nil
+}
+
+func (p *glusterfsVolumeProvisioner) CreateEpSvc(epns string, depsertoc string, hostips []string ) ( dep *api.Endpoints, dser *api.Service, err error) {
+	dep, err = p.plugin.host.GetKubeClient().Core().Endpoints(epns).Get(depsertoc)
+	if err != nil {
+		Addrlist := make([]api.EndpointAddress, len(hostips))
+		for i,v := range hostips {
+			Addrlist[i].IP = v
+		}
+		dep := &api.Endpoints{
+			ObjectMeta: api.ObjectMeta{
+				Namespace: epns,
+				Name:      depsertoc,
+			},
+			Subsets: []api.EndpointSubset{{
+				Addresses: Addrlist,
+				Ports:     []api.EndpointPort{{ Port: 1, Protocol: "TCP"}},
+			}},
+		}
+		_, err = p.plugin.host.GetKubeClient().Core().Endpoints(epns).Create(dep)
+		if err != nil {
+			glog.Errorf("glusterfs: failed to create endpoint %s", err)
+			return nil, nil, fmt.Errorf("error creating endpoint %v", err)
+		}
+	}
+	dser, err = p.plugin.host.GetKubeClient().Core().Services(epns).Get(depsertoc)
+	if err != nil {
+		dser = &api.Service{
+			ObjectMeta: api.ObjectMeta{ Name: depsertoc, Namespace: epns},
+			Spec: api.ServiceSpec{
+				Ports: []api.ServicePort{
+					{Protocol: "TCP", Port: 1}}}}
+		_, err = p.plugin.host.GetKubeClient().Core().Services(epns).Create(dser)
+		if err != nil {
+			glog.Errorf("glusterfs: failed to create service %s", err)
+			return nil, nil, fmt.Errorf("error creating service %v", err)
+		}
+	}
+	return dep,dser,nil
 }
